0ea3793f26b38aa8f0425a9bfc9eeaaccd020867
Devika Panjwani <devika.panjwani@gmail.com>
2022-09-27 01:11:51 +0530

    initial commit

---
+++ frontend/frontend/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Navbar from './components/Navbar';
+import Login from './Login';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>REACT</h1>
+
+      
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/frontend/src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Navbar.css';
+
+const Navbar = () => {
+  return (
+    <main id='login'>Navbar
+    <div class="vertical-menu">
+    <a href="#" class="active">Home</a>
+    <a href="#">Link 1</a>
+    <a href="#">Link 2</a>
+    <a href="#">Link 3</a>
+    <a href="#">Link 4</a>
+    </div>
+    </main>
+  )
+}
+
+export default Navbar
