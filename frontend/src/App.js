4a983d0ca3340def5cb51d1deab9f890092087e6
JadenFurtado <52862591+JadenFurtado@users.noreply.github.com>
2022-10-04 15:36:10 +0530

    chore: Restructuring the project

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,10 +5,9 @@ import Login from './Login';
 function App() {
   return (
     <div className="App">
+      <Navbar></Navbar>
       <h1>REACT</h1>
-
-      
-     
+      <Login></Login>
     </div>
   );
 }
