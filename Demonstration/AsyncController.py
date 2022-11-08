eb2ee78f2af9fa8c88770fcd566a736bf900703b
JadenFurtado <furtadojaden@gmail.com>
2022-11-09 00:30:03 +0530

    Adding demonstration for Presentation

--- Demonstration/AsyncController.py
+++ Demonstration/AsyncController.py
@@ -1,8 +1,8 @@
-from app import app
 from flask import jsonify
 from flask import Flask, request
 from celery import Celery
-from app.Controllers.Scanner import CodeScanner
+from Scanner import CodeScanner
+
 from celery.utils.log import get_task_logger
 import os
 from dotenv import load_dotenv
@@ -11,7 +11,9 @@ import asyncio
 load_dotenv()
 logger = get_task_logger(__name__)
 
-#app = Flask(__name__)
+app = Flask(__name__)
+
+app.config["CELERY_BROKER_URL"] = os.getenv("CELERY_BROKER_URL")
 
 celery = Celery(app.name, broker=app.config["CELERY_BROKER_URL"])
 celery.conf.update(app.config)
@@ -21,7 +23,7 @@ celery.conf.update(app.config)
 def scan(repositoryLink,path,repositoryName):
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
-    scanObj = CodeScanner(repositoryLink,path,repositoryName,finalOutput)
+    scanObj = CodeScanner(repositoryLink,path,repositoryName)
     loop.run_until_complete(scanObj.getCode())
     loop.run_until_complete(scanObj.scanCode())
     loop.run_until_complete(scanObj.cleanUp())
@@ -29,9 +31,11 @@ def scan(repositoryLink,path,repositoryName):
 
 @app.route('/')
 def add_task():
-    repositoryLink = request.get.args("repositoryLink")
-    path = request.get.args("path")
+    repositoryLink = request.args.get("repositoryLink")
+    path = request.args.get("path")
     repositoryName = request.args.get("repositoryName")
-    finalOutput = request.args.get("finalOutput")
-    scan.delay(repositoryLink,path,repositoryName,finalOutput)
+    scan.delay(repositoryLink,path,repositoryName)
     return jsonify({'status': 'ok'})
+
+if __name__=='__main__':
+    app.run()
